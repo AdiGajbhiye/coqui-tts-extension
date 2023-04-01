5be914352cc1d66dd5106a3bc7f99afc2664cc1f
Aditya <aditya.gajbhiye.20@gmail.com>
2023-04-01 16:53:17 +0100

    cleanup code

--- background.js
+++ background.js
@@ -1,13 +1,8 @@
 chrome.browserAction.onClicked.addListener((tab) => {
-  chrome.tabs.sendMessage(tab.id, { command: "getElements" }, (response) => {
+  chrome.tabs.sendMessage(tab.id, { command: "getText" }, (response) => {
     if (response) {
-      // Read the elements out loud
-      for (let i = 0; i < response.elements.length; i++) {
-        const element = response.elements[i];
-        const textToRead = `Element ${i + 1} of ${response.elements.length}. ${
-          element.textContent
-        } (${element.tagName})`;
-        // Use text-to-speech API to read the element
+      const { value: textToRead, done } = response;
+      if (!done) {
         console.log(textToRead);
         chrome.tts.speak(textToRead);
       }
--- content.js
+++ content.js
@@ -1,18 +1,17 @@
+function* textGenerator() {
+  const focusableElements = document.querySelectorAll(
+    "[data-selectable-paragraph]"
+  );
+  for (let i = 0; i < focusableElements.length; i++) {
+    const element = focusableElements[i];
+    yield element.textContent.trim();
+  }
+}
+
+const gen = textGenerator();
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.command === "getElements") {
-    const elements = [];
-    // Find all elements on the page that should be read by the screen reader
-    const focusableElements = document.querySelectorAll(
-      "a[href], [tabindex]:not([tabindex='-1']), p"
-    );
-    for (let i = 0; i < focusableElements.length; i++) {
-      const element = focusableElements[i];
-      const textContent = element.textContent.trim();
-      if (textContent !== "") {
-        elements.push({ textContent, tagName: element.tagName });
-      }
-    }
-    // Send the elements back to the background script
-    sendResponse({ elements });
+  if (message.command === "getText") {
+    sendResponse(gen.next());
   }
 });
